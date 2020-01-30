b8c73c3724b2185af76bba36c14746c5d411f1f3
ikor <igor_korenovsk@mail.ru>
2020-01-30 20:30:54 +0300

    метод симпсона

--- Program.cs
+++ Program.cs
@@ -7,22 +7,19 @@ namespace kursovoi_project_sharangiya
         static void Main(string[] args)
         {
 			//Нижний и верхний пределы интегрирования, погрешность.
-			double lowerLimit, upperLimit, calcEror;
+			double lowerLimit, upperLimit, steps;
 			Console.Write("Нижний предел интегрирования = ");
 			lowerLimit = Convert.ToDouble( Console.ReadLine());
 			Console.Write("Верхний предел интегрирования = ");
 			upperLimit = Convert.ToDouble(Console.ReadLine());
 			Console.Write("Погрешность интегрирования = ");
-			calcEror = Convert.ToDouble(Console.ReadLine());
-			//предыдущее вычисленное значение интеграла, новое значение.
-			double oldValue = calcEror + 1, newValue = 0;
+			steps = Convert.ToDouble(Console.ReadLine());
 			//N<=4 - чтобы не сравнивал на 1 шаге, т.к. newValue = 0
-			for (int N = 2; (N <= 4) || (Math.Abs(newValue - oldValue) > calcEror); N *= 2)
-			{
-				double h, sumEven = 0, sumOdd = 0, sum = 0;
+
+				double h, sumEven = 0, sumOdd = 0;
 				//Шаг интегрирования.
-				h = (upperLimit - lowerLimit) / (2 * N);
-				for (int i = 1; i <= 2 * N - 1; i += 2)
+				h = (upperLimit - lowerLimit) / (2 * steps);
+				for (int i = 1; i <= 2 * steps - 1; i += 2)
 				{
 					//Значения с нечётными индексами, которые нужно умножить на 4.
 					sumOdd += function_integral(lowerLimit + h * i);
@@ -30,11 +27,9 @@ namespace kursovoi_project_sharangiya
 					sumEven += function_integral(lowerLimit + h * (i + 1));
 				}
 				//Отнимаем значение function_integral так как ранее прибавили его дважды. 
-				sum = (upperLimit - lowerLimit)/6 *  function_integral(lowerLimit) + 4 * sumOdd + 2 * sumEven - function_integral(upperLimit);
-				oldValue = newValue;
-				newValue = (h / 3) * sum;
-			}
-			Console.WriteLine("Ответ:%d",newValue);
+
+			Console.Write("Ответ:");
+			Console.Write((upperLimit - lowerLimit) / (6 * steps) * (function_integral(lowerLimit) + 4 * sumOdd + 2 * sumEven - function_integral(upperLimit)));
 		}
 
 		static double function_integral(double x)
